c5ae1d73dddcebe0f81b0dd60d3a4e2a98e5d1e7
anmoldhami01@gmail.com <anmoldhami01@gmail.com>
2023-05-31 13:31:39 +0530

    fixed errors on build

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { HtmlHTMLAttributes } from "react";
 
 type PropsTypes = {
   params: { id: number };
@@ -13,7 +13,7 @@ interface stateInterface {
   };
 }
 
-export default function page({
+export default function Page({
   params,
 }: PropsTypes): React.FunctionComponentElement<HTMLAllCollection> {
   const [state, setState] = React.useState<stateInterface>({
@@ -22,6 +22,11 @@ export default function page({
       message: "",
     },
   });
+
+  const handleFormSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    alert(state.form.email + "is already present");
+  };
 
   return (
     <section className="text-gray-600 body-font relative">
@@ -38,7 +43,7 @@ export default function page({
       </div>
       <div className="container px-5 py-24 mx-auto flex">
         <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
-          <form>
+          <form onSubmit={handleFormSubmit}>
             <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">
               Feedback
             </h2>
